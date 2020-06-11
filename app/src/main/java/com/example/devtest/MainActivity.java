f7c1c3f8be52baf0c66536e13a80366487c4b738
Hye Wook Jung <jung_hye_wook@tech.gov.sg>
2020-06-11 17:52:05 +0800

    added location service with permissions

--- app/src/main/java/com/example/devtest/MainActivity.java
+++ app/src/main/java/com/example/devtest/MainActivity.java
@@ -2,17 +2,31 @@ package com.example.devtest;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.navigation.NavController;
@@ -21,9 +35,10 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
-
     private FusedLocationProviderClient fusedLocationProviderClient;
     private LocationRequest locationRequest;
+
+    FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,29 +53,62 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-        getLocationUpdates();
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        //getLastLocation();
+
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+//            startForegroundService(new Intent(this, LocationService.class));
+//        else
+//            startService(new Intent(this, LocationService.class));
+
+        checkPermission();
+        startService(new Intent(this, LocationService.class));
     }
 
-    public void getLocationUpdates(){
-        if (ActivityCompat.checkSelfPermission((Activity)this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions((Activity)this, new String[]{
-                    android.Manifest.permission.ACCESS_FINE_LOCATION
-            }, 10);
-        }
+    private void getLocationUpdates() {
+        checkPermission();
 
         fusedLocationProviderClient = new FusedLocationProviderClient(this);
-        locationRequest = new LocationRequest();
 
+        locationRequest = LocationRequest.create();
         locationRequest.setPriority((LocationRequest.PRIORITY_HIGH_ACCURACY));
         locationRequest.setFastestInterval(2000);
         locationRequest.setInterval(4000);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
 
         fusedLocationProviderClient.requestLocationUpdates(locationRequest, new LocationCallback() {
             @Override
             public void onLocationResult(LocationResult locationResult) {
                 super.onLocationResult(locationResult);
-                Log.d("debug", Double.toString(locationResult.getLastLocation().getLatitude()) + "," +  Double.toString(locationResult.getLastLocation().getLongitude()));
+                Log.d("debug", "1, " + Double.toString(locationResult.getLastLocation().getLatitude()) + "," + Double.toString(locationResult.getLastLocation().getLongitude()));
             }
         }, getMainLooper());
+    }
+
+    private void getLastLocation() {
+        checkPermission();
+
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            Log.d("debug", "2, " + Double.toString(location.getLatitude()) + "," + Double.toString(location.getLongitude()));
+                        }
+                    }
+                });
+    }
+
+    private void checkPermission(){
+        if (ActivityCompat.checkSelfPermission((Activity) this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions((Activity) this, new String[]{
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+            }, 10);
+        }
+
     }
 }
